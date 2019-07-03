3aab5b540f3e86073606c824218d08a196a643c2
edsonfri <edsonfri@gmail.com>
2019-07-03 23:04:12 +0100

    add projeto frontend

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2019
+            <a href="http://friedrich.pt" target="_blank"> Friedrich</a>.
+        </strong>
+    </footer>
+)
